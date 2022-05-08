81931facb41aea8fdc4f0cb27127e7699d1a6f6a
arafathossion <arafathossionrahman@gmail.com>
2022-05-08 07:27:53 +0600

    connect database and gitignore .env

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express()
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { send } = require('express/lib/response');
-
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.x8cmc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
