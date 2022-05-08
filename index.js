8c33944b8f513158a766ebcee4e2a65d3fb6c119
arafathossion <arafathossionrahman@gmail.com>
2022-05-08 07:29:45 +0600

    make run function and create database and collection

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.x8cmc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("vegetableWearhouse");
+        const vegetableItem = database.collection("vegetableItem");
 
+
+        
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
